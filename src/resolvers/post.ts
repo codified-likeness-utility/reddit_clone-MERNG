1ac610c58e3f2c298f20966d205f51f87bcf1636
Ian Davies <mr.daviesian@gmail.com>
2021-07-08 21:39:20 -0500

    User Register/Login w/Argon2 Authentication

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,52 +1,61 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
 
 @Resolver(Post)
 export class PostResolver {
-	
+	// Show all Posts
 	@Query(() => [Post])
 	posts(@Ctx() { em }: MyContext): Promise<Post[]> {
 		return em.find(Post, {});
 	}
 
-	@Query(() => Post, {nullable: true})
+	// Find one post
+	@Query(() => Post, { nullable: true })
 	post(
 		@Arg("id") _id: number,
-		@Ctx() { em }: MyContext): Promise<Post | null> {
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
 		return em.findOne(Post, { _id });
 	}
 
+	// Create new Post
 	@Mutation(() => Post)
 	async createPost(
 		@Arg("title") title: string,
-		@Ctx() { em }: MyContext): Promise<Post> {
-		const post = em.create(Post, { title })
-		await em.persistAndFlush(post)
-		return post
+		@Ctx() { em }: MyContext
+	): Promise<Post> {
+		const post = em.create(Post, { title });
+		await em.persistAndFlush(post);
+		return post;
 	}
 
-	@Mutation(() => Post, {nullable: true})
+	// Update a Post
+	@Mutation(() => Post, { nullable: true })
 	async updatePost(
 		@Arg("id") _id: number,
-		@Arg("title", () => String, {nullable: true}) title: string,
-		@Ctx() { em }: MyContext): Promise<Post | null> {
-		const post = await em.findOne(Post, { _id })
+		@Arg("title", () => String, { nullable: true }) title: string,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		const post = await em.findOne(Post, { _id });
 		if (!post) {
-			return null
+			return null;
 		}
-		if (typeof title !== 'undefined') {
-			post.title = title
-			await em.persistAndFlush(post)
+		if (typeof title !== "undefined") {
+			post.title = title;
+			await em.persistAndFlush(post);
 		}
-		return post
+		return post;
 	}
 
+	// Delete a Post
 	@Mutation(() => Boolean)
+	// Delete an existing Post
 	async deletePost(
 		@Arg("id") _id: number,
-		@Ctx() { em }: MyContext): Promise<boolean> {
-		await em.nativeDelete(Post, { _id })
-		return true
+		@Ctx() { em }: MyContext
+	): Promise<boolean> {
+		await em.nativeDelete(Post, { _id });
+		return true;
 	}
 }
